970f201ad4ea1df8f39b2313ca4bc429d1f01f96
Jaime <jamoncayop@unal.edu.co>
2022-01-10 05:52:32 -0500

    Segundo commit

--- src/getData.jsx
+++ src/getData.jsx
@@ -1,4 +1,4 @@
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, addDoc, DocumentReference,  doc, deleteDoc} from 'firebase/firestore/lite';
 import {app } from "./firebase";
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
@@ -10,6 +10,34 @@ export const db = getFirestore(app);
 export async function getUsers() {
   const usersCol = collection(db, "users");
   const usersSnapshot = await getDocs(usersCol);
-  const usersList = usersSnapshot.docs.map((doc) => doc.data());
+  const usersList = usersSnapshot.docs.map((doc) => {
+    return{
+      ...doc.data(),
+      id:doc.id
+    };
+  });
   return usersList;
+}
+
+// --------------AGREGAR USUARIOS--------------
+export async function addUsers (user){
+  try {
+    const docRef = await addDoc(collection(db, "users"), user);
+    console.log("Document written with ID: ", docRef.id);
+    return docRef.id;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return null;
+  }
+}
+// --------------ELIMINAR USUARIOS--------------
+export async function deleteUsers(id) {
+  try{
+    await deleteDoc(doc(db, "users", id));
+    return id;
+  }catch(e){
+    console.log("Enrror al borrar el item",e);
+    return null;
+  }
+  
 }
